28e22ab5cfb2342aafc79272a25f4d0ec39feb70
红豆蛋黄派 <zgpride87@outlook.com>
2023-02-27 21:17:40 +0800

    Why P2241 still hasn't ACed???how do you make me rational???
    there must be a problem in function "config"

--- P1219.cpp
+++ P1219.cpp
@@ -1,47 +1,61 @@
 #include <bits/stdc++.h>
 int n;
 int number[14][14];
+int result[14][14];
 int t = 0;
 using namespace std;
 
-void config(int x, int y,bool flag)
+void config(int x, int y,int flag)
 {
     for (int i = 1; i <= n; i++)
     {
-        number[x][i] = flag;
-        number[i][y] = flag;
+        
+        number[x][i] += flag;
+        number[i][y] += flag;
     }
+    
     if (x >= y)
     {
-        for (int i = x - y + 1; i <= n-x+y; i++)
+        for (int i =1; i <= x+y-1; i++)
         {
-            number[i][x - y + i] = flag;
-            number[i][x + y - i] = flag;
+            number[i][x+y-i]+=flag;
+        }
+        for(int i=x-y+1;i<=n;i++){
+            number[i][i-x+y]+=flag;
         }
     }
     else{
-        for(int i=y-x+1;i<=n-y+x;i++){
-            number[y-x+i][i]=flag;
-            number[x+y-i][i]=flag;
+        for(int i=1;i<=n-y+x;i++){
+            number[i][i+y-x]+=flag;
+        }
+        for(int i=x+y-n;i<=n;i++){
+            number[i][x+y-i]+=flag;
         }
     }
-    if(flag){
-        number[x][y]=2;
+    if(flag==1){
+    number[x][y]-=3;
+    }
+    if(flag==-1){
+        number[x][y]+=3;
+    }
+    if(flag==1){
+        result[x][y]=1;
+
     }
     else{
-        number[x][y]=0;
+        result[x][y]=0;
     }
 }
 void dfs(int x)
 { // x表示横向行数
-    if (x == n)
+    if (x == n+1)
     {
         if (t < 3)
         {
-            for (int i = 1; i < n; i++)
+            for (int i = 1; i <= n; i++)
             {
-                int j = 0;
-                while (j!=2)
+                int j = 1;
+                while (result[i][j]!=1)
                 {
                     j++;
                 }
@@ -59,7 +73,7 @@ void dfs(int x)
             {
                 config(x,i,1);
                 dfs(x + 1);
-                config(x,i,0);
+                config(x,i,-1);
             }
         }
     }
@@ -70,7 +84,7 @@ int main()
     freopen("P1219.in", "r", stdin);
     // freopen("P1219.out","w",stdout);
     cin >> n;
-    bool number[n][n];
+    memset(number,0,sizeof(number));
     dfs(1);
     cout << t;
     return 0;
