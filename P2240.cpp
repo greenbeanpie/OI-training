56f3a20bbc0fd6c699b7f33c2d360f83cd298601
红豆蛋黄派 <zgpride87@outlook.com>
2023-03-01 18:55:41 +0800

    Update P2240.cpp

--- P2240.cpp
+++ P2240.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 struct coin{
-    int m,v;
+    double m,v;
 };
 
 bool cmp(coin a,coin b){
@@ -13,6 +13,7 @@ bool cmp(coin a,coin b){
 }
 
 int main(){
+    freopen("P2240.in","r",stdin);
     int n,t;
     cin >> n >> t;
     coin bag[n];
@@ -20,6 +21,18 @@ int main(){
         cin >> bag[i].m >> bag[i].v;
     }
     sort(bag,bag+n,cmp);
-    
+    double summ=0;
+    double sumv=0;
+    for(int i=0;i<n;i++){
+        if(bag[i].m+summ<=t){
+            summ+=bag[i].m;
+            sumv+=bag[i].v;
+        }
+        else{
+            sumv+=(t-summ)/bag[i].m*bag[i].v;
+            break;
+        }
+    }
+    cout << fixed << setprecision(2) << sumv;
     return 0;
 }
