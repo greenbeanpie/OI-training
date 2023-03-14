ea4cfeac20c992142dd0b6270f3d7d212715d6ac
红豆蛋黄派 <zgpride87@outlook.com>
2023-03-14 21:19:48 +0800

    上传2023.3.14

--- Luogu/P5/P53/P5318/P5318.cpp
+++ Luogu/P5/P53/P5318/P5318.cpp
@@ -1,30 +1,74 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define mian main
-#define itn int
-#define tni int
-#define tin int
-#define nti int
-#define nit int
 #define int long long
 
-int sum=0;
-int count(int page){
-    if(page<10){
-        return (1+page)*page/2;
+
+struct edge{
+    int u,v;
+};
+bool cmp(edge u,edge v){
+    if(u.u>v.u){
+        return false;
     }
-    int temp=pow(10,floor(log10(page)));
-    sum+=(page/temp)*(page%temp);
-    sum+=(page/temp)*(page/temp-1)/2*temp;
-    page/=10;
-    sum+=count(page);
-    return sum;
+    else if(u.v>v.v){
+        return false;
+    }
+    return true;
+}
+vector<int> e[100005];
+int vis[100005],n,m;
+vector<edge> temp;
+void read(int t){
+    int uu,vv;
+    for(int i=0;i<t;i++){
+        cin >> uu >> vv;
+        temp.push_back((edge){uu,vv});
+    }
+    sort(temp.begin(),temp.end(),cmp);
+    for(int i=0;i<t;i++){
+        e[temp[i].u].push_back(i);
+    }
+}
+
+
+void dfs(int x){
+    vis[x]=true;
+    cout << x << " ";
+    for(int i=0;i<e[x].size();i++){
+        int t=temp[e[x][i]].v;
+        if(!vis[t]){
+        
+            dfs(t);
+        }
+    }
+}
+void bfs(int x){
+    queue<int> q;
+    q.push(x);
+    cout << x << " ";
+    vis[x]=true;
+    while(!q.empty()){
+        int t=q.front();
+        q.pop();
+        for(int i=0;i<e[t].size();i++){
+            int tt=temp[e[t][i]].v;
+            if(!vis[tt]){
+                q.push(tt);
+                cout << tt << " ";
+                vis[tt]=true;
+            }
+        }
+    }
 }
 
 signed main(){
     ios::sync_with_stdio(false);
-    int n;
-    cin >> n;
-    cout << count(n);
+    cin >> n >> m;
+    read(m);
+
+    dfs(1);
+    memset(vis,0,sizeof(vis));
+    cout << endl;
+    bfs(1);
     return 0;
 }
