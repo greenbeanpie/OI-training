a622522a2e31d876143684ebba38a6ab54accaf9
红豆蛋黄派 <zgpride87@outlook.com>
2023-03-15 21:07:44 +0800

    上传2023.3.15-2

---
+++ Luogu/P4/P49/P4999/P4999.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int unsigned long long
+#define mod 1000000007
+int l,r;
+int sum = 0;
+
+int a(int b)
+{
+    int c = b % 10 - 1;
+    return c * (c + 1) / 2;
+}
+
+int t2 = 0, t3 = 1;
+
+int count(int page)
+{
+	t2 = 0, t3 = 1,sum=0;
+    if (page <= 0)
+    {
+        return 0;
+    }
+    else if (page < 10)
+    {
+        return (1 + page) * page / 2;
+    }
+
+    while (page)
+    {
+        sum += t3 * (page / 10) * 45;
+        sum%=mod;
+        sum += t3 * a(page)%mod;
+        sum%=mod;
+		sum += (page % 10) * (t2 + 1)%mod;
+        sum%=mod;
+		t2 += (page % 10) * t3%mod;
+        t3 *= 10%mod;
+        page /= 10;
+    }
+    return sum;
+}
+
+signed main()
+{
+	int n,a,b;
+	int ans1,ans2;
+	cin>>n;
+    for(int i = 1;i<=n;i++)
+    {
+    	cin>>a>>b;
+		cout<<(count(b)%mod-count(a-1)%mod+mod)%mod<<endl;
+	}
+	return 0;
+}
--- template.cpp
+++ template.cpp
@@ -2,108 +2,160 @@
 using namespace std;
 #define int long long
 #define bimax 20000
-namespace IO {
-	template<typename _Tp>inline void read(_Tp& x) {
-		#define gc() getchar()
-		x = 0; char c = gc(); bool f = 0;
-		while(!std::isdigit(c)) f |= c == 45, c = gc();
-		while(std::isdigit(c)) x = x * 10 + (c ^ 48), c = gc();
+namespace IO
+{
+	template <typename _Tp>
+	inline void read(_Tp &x)
+	{
+#define gc() getchar()
+		x = 0;
+		char c = gc();
+		bool f = 0;
+		while (!std::isdigit(c))
+			f |= c == 45, c = gc();
+		while (std::isdigit(c))
+			x = x * 10 + (c ^ 48), c = gc();
 		return (void)(f ? x = -x : 1);
 	}
-	
-	template<typename _Tp>inline void write(_Tp x) {
-		static char stk[20]; int top = 0;
-		if(!x) return putchar(48), void();
-		if(x < 0) putchar(45), x = -x;
-		while(x) stk[top++] = x % 10, x /= 10;
-		while(top) putchar(stk[--top] + 48);
+
+	template <typename _Tp>
+	inline void write(_Tp x)
+	{
+		static char stk[20];
+		int top = 0;
+		if (!x)
+			return putchar(48), void();
+		if (x < 0)
+			putchar(45), x = -x;
+		while (x)
+			stk[top++] = x % 10, x /= 10;
+		while (top)
+			putchar(stk[--top] + 48);
 	}
-	
-	template<typename _Tp, typename ...Args>
-	inline void read(_Tp& x, Args& ...args) {
-        read(x), read(args...);
-    }
+
+	template <typename _Tp, typename... Args>
+	inline void read(_Tp &x, Args &...args)
+	{
+		read(x), read(args...);
+	}
 };
 
-
-struct bigint {
-	int len=0;
-	int num[bimax]= {0};
-	bigint(int x=0) {
-		for(int i=0; x; i++) {
-			num[i]=x%10;
-			x/=10;
+struct bigint
+{
+	int len = 0;
+	int num[bimax] = {0};
+	bigint(int x = 0)
+	{
+		for (int i = 0; x; i++)
+		{
+			num[i] = x % 10;
+			x /= 10;
 			len++;
 		}
-		if(num[len]!=0) {
+		if (num[len] != 0)
+		{
 			len++;
 		}
 	}
-	int &operator[](int a) {
+	int &operator[](int a)
+	{
 		return num[a];
 	}
-	void print() {
-		for(int i=len-1; i>=0; i--) {
+	void print()
+	{
+		for (int i = len - 1; i >= 0; i--)
+		{
 			cout << num[i];
 		}
 	}
-	void flatten(int L){
-		len=L;
-		for(int i=0;i<L;i++)
+	void flatten(int L)
+	{
+		len = L;
+		for (int i = 0; i < L; i++)
 		{
-			if(num[i]>=10)
+			if (num[i] >= 10)
 			{
-				num[i+1]+=num[i]/10;
-				num[i]%=10;
+				num[i + 1] += num[i] / 10;
+				num[i] %= 10;
 			}
-
+			if (num[i] < 0)
+			{
+				num[i + 1] += floor(1.0 * num[i] / 10);
+				num[i] %= 10;
+			}
 		}
-		while(!num[len]){
+		while (!num[len])
+		{
 			len--;
-		} 
+		}
 		len++;
-		if(len==0){
+		if (len == 0)
+		{
 			len++;
 		}
-	} 
-
+	}
 };
-bigint operator+(bigint a,bigint b)
+bigint operator+(bigint a, bigint b)
 {
-	bigint c=0;
-	for(int i=0; i<max(a.len,b.len); i++) {
-		c[i]+=a[i]+b[i];
-		if(c[i]>=10) {
-			c[i+1]+=c[i]/10;
-			c[i]%=10;
-		}
-	}
-	c.flatten(max(a.len,b.len)+1);
-	return c;
-}
-bigint operator*(bigint a,bigint b)
-{
-	bigint c=0;
-	for(int i=0; i<a.len; i++) {
-		for(int j=0;j<b.len;j++)
+	bigint c = 0;
+	for (int i = 0; i < max(a.len, b.len); i++)
+	{
+		c[i] += a[i] + b[i];
+		if (c[i] >= 10)
 		{
-			c[i+j]+=a[i]*b[j];
+			c[i + 1] += c[i] / 10;
+			c[i] %= 10;
 		}
 	}
-	c.flatten(a.len+b.len+1);
+	c.flatten(max(a.len, b.len) + 1);
 	return c;
 }
-bigint stb(string x,bigint a)
+bigint operator-(bigint a, bigint b)
 {
-	for(int i=x.length()-1; i>=0; i--) {
-		a[i]=x[x.length()-1-i]-'0';
+	bigint c = 0;
+	for (int i = 0; i < max(a.len, b.len); i++)
+	{
+		c[i] += a[i] - b[i];
+		if (c[i] < 0)
+		{
+			c[i + 1] += floor(1.0 * c[i] / 10);
+			c[i] %= 10;
+		}
+	}
+	c.flatten(max(a.len, b.len) + 1);
+	return c;
+}
+bigint operator*(bigint a, bigint b)
+{
+	bigint c = 0;
+	for (int i = 0; i < a.len; i++)
+	{
+		for (int j = 0; j < b.len; j++)
+		{
+			c[i + j] += a[i] * b[j];
+		}
+	}
+	c.flatten(a.len + b.len + 1);
+	return c;
+}
+bigint stb(string x)
+{
+	bigint a(0);
+	for (int i = x.length() - 1; i >= 0; i--)
+	{
+		a[i] = x[x.length() - 1 - i] - '0';
 		a.len++;
 	}
 	return a;
 }
-using namespace IO;
-signed main(){
-    ios::sync_with_stdio(false);
 
-    return 0;
+using namespace IO;
+signed main()
+{
+	ios::sync_with_stdio(false);
+	bigint a(2),b(1);
+	bigint c;
+	c=a-b;
+	c.print();
+	return 0;
 }
