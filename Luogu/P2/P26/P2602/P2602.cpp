185ef5245103a39148460278b9894e9eca876f45
红豆蛋黄派 <zgpride87@outlook.com>
2023-03-19 21:20:40 +0800

    上传2023.3.19

--- Luogu/P2/P26/P2602/P2602.cpp
+++ Luogu/P2/P26/P2602/P2602.cpp
@@ -6,11 +6,7 @@ int n, a1, b1;
 int sum = 0;
 int dabiao[13] = {0, 1, 20, 300, 4000, 50000, 600000, 7000000, 80000000, 900000000, 10000000000, 110000000000, 1200000000000};
 int c1[13] = {0};
-int a(int b)
-{
-    int c = b % 10 - 1;
-    return c * (c + 1) / 2;
-}
+
 int getlast(int a)
 {
     return a % 10;
@@ -24,45 +20,61 @@ int count(int page, int num)
     if (num == 0)
     {
     }
-    t2 = 0;
-    sum = 0;
     int temp = 0;
+    int t2 = 0;
+    int sum = 0;
+    int c1[13] = {0};
     while (page)
     {
-        temp = c1[t2 - 1];
         if (getlast(page) > num)
         {
-            
-            if (t2 == 0)
-            {
-                temp++;
+            temp += pow(10, t2);
 
-                temp += opage - page * pow(10, t2);
-            }
-            else
-            {
-                
-                temp += opage - page * pow(10, t2);
-            }
+            temp += (dabiao[t2]) * (getlast(page));
+            // temp+=c1[max(t2-1,(long long)0)];
+        }
+        else if (getlast(page) == num)
+        {
+            temp++;
+            temp += opage - page * pow(10, t2);
+            temp += (dabiao[max(t2, (long long)0)]) * (getlast(page));
         }
         else
         {
-            temp += opage - page * pow(10, t2) + 1;
+            temp += c1[max(t2 - 1, (long long)0)];
+            temp += (dabiao[max(t2 , (long long)0)]) * (getlast(page));
+            if(t2>0&&opage/(int)(pow(10,t2-1))%10>=num){
+                temp-=c1[t2-1];
+            }
         }
-        temp += (getlast(page)-1) * dabiao[(int)(floor(log10(opage)) - floor(log10(page)))];
-        c1[t2] = temp;
         sum += temp;
+        c1[t2] = temp;
         page /= 10;
         t2++;
+        temp = 0;
+    }
+    if(num==0){
+        int t=0;
+        while(t2){
+            t=t*10+1;
+            t2--;
+        }
+        sum-=t;
+        return sum;
     }
     return sum;
 }
 
 signed main()
 {
-
+#ifndef ONLINE_JUDGE
+    freopen("P2602.in", "r", stdin);
+#endif
     cin >> a1 >> b1;
-    cout << count(b1, 1) - count(a1, 1) << endl;
-
+    for (int i = 0; i <= 9; i++)
+    {
+        cout << count(b1, i) - count(a1 - 1, i) << " ";
+    }
+    cout << endl;
     return 0;
 }
