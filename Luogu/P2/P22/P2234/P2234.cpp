b7c78e850c88c629ea1c0849f7c3efac7e3f8258
红豆蛋黄派 <zgpride87@outlook.com>
2023-03-22 21:21:15 +0800

    上传2023.3.22

--- Luogu/P2/P22/P2234/P2234.cpp
+++ Luogu/P2/P22/P2234/P2234.cpp
@@ -2,33 +2,34 @@
 using namespace std;
 #define int long long
 int temp;
-struct a{
-    int day,num;
-};
-bool operator<(a x,a y){
-    return x.num<y.num;
-}
 
-set<int> s;
 signed main(){
+    ios::sync_with_stdio(false);
+    set<int> s;
     int n;
     cin >> n;
     int sum=0;
+    s.insert(INT_MIN);
+    s.insert(INT_MAX);
     for(int i=0;i<n;i++){
         cin >> temp;
-        if(s.empty()){
+        if(s.size()==2){
             sum+=temp;
             s.insert(temp);
         }
         else{
             auto t=s.lower_bound(temp);
+            
             if(*t==temp){
                 continue;
             }
-            sum+=min(abs(*t-temp),abs(*(--t)-temp));
+
+            auto t1=t;
+            t1--;
+            sum+=min(abs(*t-temp),abs(*t1-temp));
             s.insert(temp);
         }
     }
-    cout << sum-1;
+    cout << sum;
     return 0;
 }
