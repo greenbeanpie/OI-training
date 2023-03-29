bb13572f63cd83f389a8b8e062b2c708f48f0f75
Mozhi Hong <zgpride87@gmail.com>
2023-03-29 21:13:30 +0800

    上传2023.3.29

--- Luogu/P3/P33/P3390/P3390.cpp
+++ Luogu/P3/P33/P3390/P3390.cpp
@@ -2,37 +2,40 @@
 using namespace std;
 #define int long long
 #define max_matrix 105
+#define mod 1000000007
 
 int n, k;
 struct matrix {
   long long mat[max_matrix][max_matrix] = {0};
   void init() {
-    for (int i = 0; i < max_matrix; i++) {
+    for (int i = 1; i <= max_matrix; i++) {
       mat[i][i] = 1;
     }
   }
 } a;
 matrix operator*(matrix a, matrix b) {
   matrix c;
-  for (int k = 0; k < n; k++) {
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
-        c.mat[i][j] += a.mat[i][k] + b.mat[k][j];
+  for (int k = 1; k <= n; k++) {
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= n; j++) {
+        c.mat[i][j] += (a.mat[i][k] * b.mat[k][j]) % mod;
+        c.mat[i][j] %= mod;
       }
     }
   }
   return c;
 }
-matrix mquickpower(matrix a, matrix b) {
+matrix mquickpower(matrix a, int b) {
+  matrix answer;
+  answer.init();
   if (b == 0) {
-    return 1;
+    return answer;
   }
-  long long answer = 1;
   while (b) {
     if (b & 1) {
-      answer = answer * a % p;
+      answer = answer * a;
     }
-    a = a * a % p;
+    a = a * a;
     b >>= 1;
   }
   return answer;
@@ -47,6 +50,12 @@ signed main() {
       cin >> a.mat[i][j];
     }
   }
-
+  mquickpower(a, k);
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      cout << a.mat[i][j] % mod << " ";
+    }
+    cout << endl;
+  }
   return 0;
 }
