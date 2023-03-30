9d95f037c14543423d8aa660f94b4bbffca4cdf3
Mozhi Hong <zgpride87@gmail.com>
2023-03-30 21:20:21 +0800

    from-next-commit

--- Luogu/P3/P33/P3390/P3390.cpp
+++ Luogu/P3/P33/P3390/P3390.cpp
@@ -1,61 +1,60 @@
-#include <bits/stdc++.h>
+#include<algorithm>
+#include<iostream>
+#include<cstring>
+#include<cstdio>
+#include<cctype>
+#define ll long long
+#define gc() getchar()
+#define maxn 105
+#define mo 1000000007
 using namespace std;
-#define int long long
-#define max_matrix 105
-#define mod 1000000007
 
-int n, k;
-struct matrix {
-  long long mat[max_matrix][max_matrix] = {0};
-  void init() {
-    for (int i = 1; i <= max_matrix; i++) {
-      mat[i][i] = 1;
-    }
-  }
-} a;
-matrix operator*(matrix a, matrix b) {
-  matrix c;
-  for (int k = 1; k <= n; k++) {
-    for (int i = 1; i <= n; i++) {
-      for (int j = 1; j <= n; j++) {
-        c.mat[i][j] += (a.mat[i][k] * b.mat[k][j]) % mod;
-        c.mat[i][j] %= mod;
-      }
-    }
-  }
-  return c;
+inline ll read(){
+	ll a=0;int f=0;char p=gc();
+	while(!isdigit(p)){f|=p=='-';p=gc();}
+	while(isdigit(p)){a=(a<<3)+(a<<1)+(p^48);p=gc();}
+	return f?-a:a;
 }
-matrix mquickpower(matrix a, int b) {
-  matrix answer;
-  answer.init();
-  if (b == 0) {
-    return answer;
-  }
-  while (b) {
-    if (b & 1) {
-      answer = answer * a;
-    }
-    a = a * a;
-    b >>= 1;
-  }
-  return answer;
+int n;
+
+struct ahaha{
+	ll a[maxn][maxn];     //一定要用long long存矩阵，否则在过程中会爆掉
+	ahaha(){
+		memset(a,0,sizeof a);
+	}
+	inline void build(){     //建造单位矩阵
+		for(int i=1;i<=n;++i)a[i][i]=1;
+	}
+}a;
+ahaha operator *(const ahaha &x,const ahaha &y){     //重载运算符
+	ahaha z;
+	for(int k=1;k<=n;++k)
+		for(int i=1;i<=n;++i)
+			for(int j=1;j<=n;++j)
+				z.a[i][j]=(z.a[i][j]+x.a[i][k]*y.a[k][j]%mo)%mo;
+	return z;
 }
-signed main() {
-  ios::sync_with_stdio(false);
-  cin >> n >> k;
-  matrix a;
-  a.init();
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-      cin >> a.mat[i][j];
-    }
-  }
-  mquickpower(a, k);
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-      cout << a.mat[i][j] % mod << " ";
-    }
-    cout << endl;
-  }
-  return 0;
+
+ll k;
+inline void init(){
+	n=read();k=read();
+	for(int i=1;i<=n;++i)
+		for(int j=1;j<=n;++j)
+			a.a[i][j]=read();
+}
+
+int main(){
+	init();
+  #ifndef ONLINE_JUDGE
+    freopen("P3390_1.in","r",stdin);
+  #endif
+	ahaha ans;ans.build();
+	do{
+		if(k&1)ans=ans*a;
+		a=a*a;k>>=1;
+	}while(k);
+	for(int i=1;i<=n;putchar('\n'),++i)
+		for(int j=1;j<=n;++j)
+			printf("%d ",ans.a[i][j]);
+	return 0;
 }
