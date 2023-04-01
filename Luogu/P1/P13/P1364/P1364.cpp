f8c182531b562c28281abc21f7a837ec6bf200f7
Mozhi Hong <zgpride87@outlook.com>
2023-04-02 07:51:22 +0800

    Upload P1364.cpp

--- Luogu/P1/P13/P1364/P1364.cpp
+++ Luogu/P1/P13/P1364/P1364.cpp
@@ -1,51 +1,65 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <queue>
 using namespace std;
-int n,m,mininum=INT_MAX;
-
-struct node{
-    int father=-1,lson=-1,rson=-1,weight=-1;
-}p[105];
-bool vis[105];
-int dfs(int point,int croad,int people,int target){
-    if(point==target){
-        return croad;
-    }
-    int minroad=INT_MAX;
-    if(p[point].father!=-1&&!vis[p[point].father]){
-        vis[p[point].father]=1;
-        minroad=min(minroad,dfs(p[point].father,croad+people,people,target));
-        vis[p[point].father]=0;
-    }
-    if(p[point].lson!=-1&&!vis[p[point].lson]){
-        vis[p[point].lson]=1;
-        minroad=min(minroad,dfs(p[point].lson,croad+people,people,target));
-        vis[p[point].lson]=0;
-    }
-    if(p[point].rson!=-1&&!vis[p[point].rson]){
-        vis[p[point].rson]=1;
-        minroad=min(minroad,dfs(p[point].rson,croad+people,people,target));
-        vis[p[point].rson]=0;
-    }
-    return minroad;
-}
-int main(){
-    #ifndef ONLINE_JUDGE
-        freopen("P1364_3.in","r",stdin);
-    #endif
-    
-    cin >> n;
-    for(int i=1;i<=n;i++){
-        cin >> p[i].weight >> p[i].lson >> p[i].rson;
-        p[p[i].lson].father=i;
-        p[p[i].rson].father=i;
-    }
-    for(int i=1;i<=n;i++){
-        int tempsum=0;
-        for(int j=1;j<=n;j++){
-            tempsum+=dfs(j,0,p[j].weight,i);
+int n, num, lc, rc, ans = 100000000;
+struct city
+{
+    int pe,l,r,father,weight,step;
+};
+city a1[110];
+int bfs(int aim)
+{
+    int vis[110] = {};
+    int sum = 0;
+    queue<city> q;
+    vis[aim] = 1;
+    q.push(a1[aim]);
+    while (!q.empty())
+    {
+        city temp = q.front();
+        if (temp.l && !vis[temp.l])
+        {
+            q.push(a1[temp.l]);
+            a1[temp.l].step = temp.step + 1;
+            sum += a1[temp.l].pe * a1[temp.l].step;
         }
-        mininum=min(mininum,tempsum);
+        if (temp.r && !vis[temp.r])
+        {
+            q.push(a1[temp.r]);
+            a1[temp.r].step = temp.step + 1;
+            sum += a1[temp.r].pe * a1[temp.r].step;
+        }
+        if (temp.father && !vis[temp.father])
+        {
+            q.push(a1[temp.father]);
+            a1[temp.father].step = temp.step + 1;
+            sum += a1[temp.father].pe * a1[temp.father].step;
+        }
+        cout << temp.weight << " ";
+        vis[temp.weight] = 1;
+        q.pop();
     }
-    cout << mininum << endl;
-    return 0;
+    return sum;
+}
+int main()
+{
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> num >> lc >> rc;
+        a1[i].pe = num;
+        a1[i].l = lc;
+        a1[i].r = rc;
+        a1[rc].father = i;
+        a1[lc].father = i;
+        a1[i].weight = i;
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            a1[i].step = 0;}
+        ans = min(ans, bfs(i));
+    }
+    cout << ans;
 }
