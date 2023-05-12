4a18f59ccfc6caa8c08d2289e0d37454c573000d
红豆蛋黄派 <zgpride87@outlook.com>
2023-05-12 11:24:25 +0000

    上传2023.5.12比赛 Div.2 270/400 Div.1 100/400

--- Luogu/P2/P28/P2880/P2880.cpp
+++ Luogu/P2/P28/P2880/P2880.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 int a_max[50005][17], a_min[50005][17];
 
-int rmq(bool op, int l, int r)
+int rmq(bool op, int l, int r)// ST表使用了倍增思想，目的是使用两个长度为2^n(n∈N)的区间覆盖整个查询区间，预处理时间复杂度O(nlog2n)，查询时间复杂度O(1)
 { // op==0==>query maxinum op==1==>query mininum
-    int t = floor(log2(r - l + 1));
+    int t = floor(log2(r - l + 1));// 计算n为多少时可以正好覆盖区间
     if (op == 0)
     {
-        return max(a_max[l][t], a_max[r - (1 << t) + 1][t]);
+        return max(a_max[l][t], a_max[r - (1 << t) + 1][t]);// 对于区间使右边的长度为2^n的部分和左边的2^n部分正好可以覆盖整个区间，此时查询既不会有遗漏也不会有溢出
     }
     else
     {
@@ -33,8 +33,8 @@ signed main()
     {
         for (int i = 1; i+(1<<j)-1<=n; i++)
         {
-            a_max[i][j] = max(a_max[i + (1 << (j - 1))][j - 1], a_max[i][j - 1]);
-            a_min[i][j] = min(a_min[i + (1 << (j - 1))][j - 1], a_min[i][j - 1]);
+            a_max[i][j] = max(a_max[i + (1 << (j - 1))][j - 1], a_max[i][j - 1]);// 对于i + (1 << (j - 1))~i+(1<<j)和i~i + (1 << (j - 1))两个区间的最大值取较大值
+            a_min[i][j] = min(a_min[i + (1 << (j - 1))][j - 1], a_min[i][j - 1]);// 同理
         }
     }
     for (int i = 1; i <= m; i++)
