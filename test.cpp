ef3e25bfd722ffa0bbefc314b5a8120bb054dfe6
红豆蛋黄派 <zgpride87@outlook.com>
2023-07-01 21:41:55 +0800

    上传2023.7.1-2

--- test.cpp
+++ test.cpp
@@ -1,9 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-   for(int i=0;i<100000;i++){
-    printf("\n");
-   }
+int main()
+{
+	set<int> s;
+	s.insert(1);
+	s.insert(3);
+	cout << *s.erase(s.end());
     return 0;
 }
