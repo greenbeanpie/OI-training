d535a25f6de063091ebc326b9ef8bfb2c624e52b
Mozhi Hong <zgpride87@outlook.com>
2024-10-25 15:20:03 +0800

    Upload 2024.10.25-2

--- Luogu/P3/P33/P3388/P3388.cpp
+++ Luogu/P3/P33/P3388/P3388.cpp
@@ -1,33 +1,38 @@
 #include <bits/stdc++.h>
-#include <bits/extc++.h>
-using namespace __gnu_pbds;
-using namespace __gnu_cxx;
+// #include <bits/extc++.h>
+// using namespace __gnu_pbds;
+// using namespace __gnu_cxx;
 using namespace std;
 #define int long long
 #define endl "\n"
 const int N = 2e4 + 5;
-int vdcc,dfn[N], low[N], vis[N], nowdfn, cnt;
+int vdcc, dfn[N], low[N], vis[N], nowdfn, cnt;
 vector<int> e[N];
-void tarjan(int u,bool isroot=false)
+void tarjan(int u, bool isroot = false)
 {
-	int son=0;
-	dfn[u]=low[u]=++nowdfn;
-	for(int i:e[u]){
-		if(!dfn[i]){
+	int son = 0;
+	dfn[u] = low[u] = ++nowdfn;
+	for (int i : e[u])
+	{
+		if (!dfn[i])
+		{
 			son++;
 			tarjan(i);
-			low[u]=min(low[u],low[i]);
-			if(!isroot&&dfn[u]<=low[i]&&!vis[u]){
-				vis[u]=true;
+			low[u] = min(low[u], low[i]);
+			if (!isroot && dfn[u] <= low[i] && !vis[u])
+			{
+				vis[u] = true;
 				vdcc++;
 			}
 		}
-		else{
-			low[u]=min(low[u],dfn[i]);
+		else
+		{
+			low[u] = min(low[u], dfn[i]);
 		}
 	}
-	if(son>=2&&isroot&&!vis[u]){
-		vis[u]=true;
+	if (son >= 2 && isroot && !vis[u])
+	{
+		vis[u] = true;
 		vdcc++;
 	}
 }
@@ -40,23 +45,28 @@ signed main()
 #endif
 	ios::sync_with_stdio(false);
 	cin.tie(0), cout.tie(0);
-	int n,m;
+	int n, m;
 	cin >> n >> m;
-	int u,v;
+	int u, v;
 
-	for(int i=1;i<=m;i++){
+	for (int i = 1; i <= m; i++)
+	{
 		cin >> u >> v;
 		e[u].push_back(v);
 		e[v].push_back(u);
 	}
-	for(int i=1;i<=n;i++){
-		if(!dfn[i]){
-			tarjan(i,true);
+	for (int i = 1; i <= n; i++)
+	{
+		if (!dfn[i])
+		{
+			tarjan(i, true);
 		}
 	}
 	cout << vdcc << endl;
-	for(int i=1;i<=n;i++){
-		if(vis[i]){
+	for (int i = 1; i <= n; i++)
+	{
+		if (vis[i])
+		{
 			cout << i << " ";
 		}
 	}
